0ee390c63b9e87c57c7001c17eaac8b575d9e6ca
sarthak sachdeva <sarthak.sachdeva.73@gmail.com>
2022-12-26 04:07:11 +0530

    :lock: add rate limiter

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import config from "../config/config";
 import routes from "../api/index";
+import { rateLimit } from "express-rate-limit";
 
 export default ({ app }: { app: express.Application }): void => {
   app.use(cors());
@@ -10,6 +11,15 @@ export default ({ app }: { app: express.Application }): void => {
 
   app.use(config.api.prefix, routes());
 
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 15000,
+    message: "Too many api requests, please try again later.",
+    standardHeaders: true,
+    legacyHeaders: false,
+  });
+
+  app.use(limiter);
   //when we use next(err) it will go to error handling middleware and it will catch error and send response.
   app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     res.status(err.statusCode || 500).json({
